04db8ffcb8a72e3a2a73a1f6773b5ef187ce4190
Rayman-bat9 <gaga63354@gmail.com>
2024-07-28 17:43:47 +0300

    run prettier write

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from '@eslint/js'
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
     js.configs.recommended,
