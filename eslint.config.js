b38fc04ff05c35b9f69a681a000faf545a920b69
Rayman-bat9 <gaga63354@gmail.com>
2024-07-28 15:07:22 +0300

    eslint and prettier fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,21 @@
-import js from "@eslint/js";
-import prettier from "prettier"
+import js from '@eslint/js'
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-  js.configs.recommended,
-  prettier,
-
-  {
-    rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
+    js.configs.recommended,
+    eslintConfigPrettier,
+    {
+        rules: {
+            'no-unused-vars': 'warn',
+            'no-undef': 'warn',
+        },
     },
-  },
 
-  {
-    ignores: [
-      "!node_modules/", // unignore `node_modules/` directory
-      "node_modules/*", // ignore its content
-      "!node_modules/mylibrary/", // unignore `node_modules/mylibrary` directory
-    ],
-  },
-];
+    {
+        ignores: [
+            '!node_modules/', // unignore `node_modules/` directory
+            'node_modules/*', // ignore its content
+            '!node_modules/mylibrary/', // unignore `node_modules/mylibrary` directory
+        ],
+    },
+]
