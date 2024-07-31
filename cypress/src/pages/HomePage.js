34b1732055ce734691a381e1c8a0dd4d52d367f9
Rayman-bat9 <gaga63354@gmail.com>
2024-07-31 12:24:35 +0300

    fix prettier

--- cypress/src/pages/HomePage.js
+++ cypress/src/pages/HomePage.js
@@ -5,7 +5,7 @@ import LogInModal from '../modals/LogInModal'
 export default class HomePage extends BasePage {
     header = new Header()
     logInModal = new LogInModal()
-    
+
     constructor() {
         super('index.html')
     }
