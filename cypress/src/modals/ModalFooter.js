f12eb574f2a07de4bdc9a6ecdbb71fe72326c31b
Maryna Mala <marinamalaa0@gmail.com>
2024-08-02 10:25:42 +0300

    BaseModal.js refactored

---
+++ cypress/src/modals/ModalFooter.js
@@ -0,0 +1,19 @@
+/*globals cy*/
+export default class ModalFooter {
+    constructor(footerContainer = '.modal-footer') {
+        this.FOOTER_CONTAINER = footerContainer;
+    }
+
+    get footerCloseModalButton() {
+        return cy.get(`${this.FOOTER_CONTAINER} > .btn-secondary`);
+    }
+
+    get modalFooterButton() {
+        return cy.get(`${this.FOOTER_CONTAINER} > .btn-primary`);
+    }
+
+    checkFooterCloseButton() {
+        this.footerCloseModalButton.contains('Close');
+        return this;
+    }
+}
---
+++ cypress/src/modals/ModalHeader.js
@@ -0,0 +1,24 @@
+/*globals cy*/
+export default class ModalHeader {
+    constructor(headerContainer = '.modal-header') {
+        this.HEADER_CONTAINER = headerContainer;
+    }
+
+    get modalHeaderText() {
+        return cy.get(`${this.HEADER_CONTAINER} .modal-title`);
+    }
+
+    get headerCloseModalButton() {
+        return cy.get(`${this.HEADER_CONTAINER} .close`);
+    }
+
+    checkTextInHeader(headerText) {
+        this.modalHeaderText.contains(headerText);
+        return this;
+    }
+
+    checkHeaderCloseButton() {
+        this.headerCloseModalButton.should('be.visible');
+        return this;
+    }
+}
