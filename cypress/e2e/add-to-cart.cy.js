776982c0d947adad894df3413dea330cc7f2ce77
Maryna Mala <marinamalaa0@gmail.com>
2024-08-05 18:26:14 +0300

    add-to-cart modified

--- cypress/e2e/add-to-cart.cy.js
+++ cypress/e2e/add-to-cart.cy.js
@@ -2,12 +2,10 @@ import { homePage } from "../src/pages";
 import { getProductName } from '../src/services/productService.js'
 
 describe('Add to cart', () => {
-
-    const productName = getProductName(); 
     
     it('Check "Add to cart" functionality', () => {
         cy.loginWithValidCredentials();
-        homePage.productCard.clickOnProductByName(productName);
+        homePage.productCard.clickOnProductByName(getProductName());
     })
 
 
